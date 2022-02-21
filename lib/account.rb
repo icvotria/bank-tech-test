5a3d7ad48a66f461e8c65416abb3dd0f8e947645
icvotria <icvotria@gmail.com>
2022-02-21 16:39:39 +0000

    refactoring of account class, account initialised with statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,19 +1,28 @@
 require_relative 'transaction'
+require_relative 'statement'
 
 class Account
   attr_reader :balance
 
   def initialize
+    @statement = Statement.new
     @balance = 0
   end
 
   def deposit(amount)
     @balance += amount.to_i
-    transaction = Transaction.new(amount)
+    add_to_statement(amount)
   end
 
   def withdrawal(amount)
     @balance -= amount.to_i
-    transaction = Transaction.new(-amount)
+    add_to_statement(-amount)
+  end
+
+  private
+
+  def add_to_statement(amount)
+    transaction = Transaction.new(amount)
+    @statement.add_transaction(transaction)
   end
 end
