aa96549323b84b1e7fb8f5c778efdc07502a37a9
icvotria <icvotria@gmail.com>
2022-02-21 16:25:43 +0000

    array test for statement

---
+++ spec/statement_spec.rb
@@ -0,0 +1,14 @@
+require 'statement'
+
+describe Statement do
+  let(:statement) { Statement.new }
+  let(:transaction1) { { amount: 100, date: 2022-02-21, type: 'credit' } }
+  let(:transaction2) { { amount: 50, date: 2022-02-22, type: 'debit' } }
+
+  it 'can return an array of transactions' do
+    statement.add_transaction(transaction1)
+    statement.add_transaction(transaction2)
+
+    expect(statement.transaction_list.length).to eq 2
+  end
+end
