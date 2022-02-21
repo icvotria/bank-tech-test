f433c5813fb9168ee343207771ac0d7db5afbfe5
icvotria <icvotria@gmail.com>
2022-02-21 13:13:30 +0000

    withdrawal method and test

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,15 @@
 class Account
   attr_reader :balance
-  
+
   def initialize
     @balance = 0
   end
 
   def deposit(amount)
     @balance += amount.to_i
+  end
+
+  def withdrawal(amount)
+    @balance -= amount.to_i
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,10 @@ describe Account do
 
     expect(account.balance).to eq 100
   end
+
+  it 'accepts withdrawals' do
+    account.withdrawal('50')
+
+    expect(account.balance).to eq -50
+  end
 end
