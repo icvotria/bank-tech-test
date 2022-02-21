8d5e6252150e0297cc80938185df870604d4c5e8
icvotria <icvotria@gmail.com>
2022-02-21 16:36:06 +0000

    statement class filled out a bit

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,2 +1,11 @@
 class Statement
+  attr_reader :transaction_list
+
+  def initialize
+    @transaction_list = []
+  end
+
+  def add_transaction(transaction)
+    @transaction_list << transaction
+  end
 end
